fac7551cfa2f7bc6d8f3e8d790e7de566765ec5e
RiyaMalik <157145434+Riyaa6281@users.noreply.github.com>
2024-08-14 01:07:22 +0500

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,27 @@
+// <!-- Problem #01 -->
+    
+
+        var age = 20;
+         alert("I am 20 years old.")
+      
+         var visitTime = 5;
+         alert("you have visited this site"+" "+visitTime+" "+"times.");
+
+    //   Problem #02
+    
+    
+        var birthYear = 2004;
+        document.write("My birth year is"+" "+birthYear+"<br>" );
+        document.write("Data type of my declared variable is number </br>")
+
+
+   
+    //  <!-- Problem #03 -->
+
+    
+    var _name = "Riya Habib";
+    var _title = "T-shirt(s)";
+    var _quantity = 5;
+    document.write("<b>"+_name+"</b>"+" ordered "+"<b>" + _quantity +" " +_title+"</b>"+ " on XYZ Clothing store.");
+
+ 
